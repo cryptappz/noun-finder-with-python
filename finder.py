bda58ff7e06badb800651902c69471eecbe001b3
ARNAB MUNSHI <arnab47stars@gmail.com>
2020-06-26 17:43:39 +0530

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,5 +1,7 @@
-from textblob import TextBlob
+import nltk
 
-txt = """Natural language processing (NLP) is a field of computer science, artificial intelligence, and computational linguistics concerned with the interactions between computers and human (natural) languages."""
-blob = TextBlob(txt)
-print(blob.noun_phrases)
+lines = 'Natural language processing (NLP) is a field of computer science, artificial intelligence, and computational linguistics concerned with the interactions between computers and human (natural) languages'
+
+tokenized = nltk.word_tokenize(lines)
+nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if(pos[:2] == 'NN')]
+print (nouns)
